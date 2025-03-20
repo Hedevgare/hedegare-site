13fd90dff574e020a41c2876de38e4d7d654d32c
Edgar Moreira <hedegaremoreira@gmail.com>
2025-03-20 02:08:16 +0000

    refactor: Change layout and styling
    This commit refactors the layout of the website along with its styling;
    its also adds a listing of the projects available to display.

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const BASE_TEXT = "Edgar";
-const NICK_TEXT = "\"Hedegare\"";
+// const BASE_TEXT = "Edgar";
+// const NICK_TEXT = "\"Hedegare\"";
 
-var nicknameElement = document.getElementById("nickname");
+// var nicknameElement = document.getElementById("nickname");
 
-document.getElementById("title").addEventListener("mouseover", (event) => {
-    nicknameElement.innerText = NICK_TEXT;
-});
+// document.getElementById("title").addEventListener("mouseover", (event) => {
+//     nicknameElement.innerText = NICK_TEXT;
+// });
 
-document.getElementById("title").addEventListener("mouseleave", (e) => {
-    nicknameElement.innerText = BASE_TEXT;
-});
+// document.getElementById("title").addEventListener("mouseleave", (e) => {
+//     nicknameElement.innerText = BASE_TEXT;
+// });
 
 var images = [
-    'bg-1.svg',
-    'bg-2.svg',
-    'bg-3.svg',
-    'bg-4.svg',
-    'bg-5.svg',
+    // 'bg-1.svg',
+    'bg-6.svg',
+    // 'bg-3.svg',
+    // 'bg-4.svg',
+    // 'bg-5.svg',
 ];
 
 function getRandomBackgroundImage() {
-    var randomNumber = Math.floor(Math.random() * 5);
+    var randomNumber = Math.floor(Math.random() * 1);
 
     document.getElementById("bg-image").setAttribute("src", "images/" + images[randomNumber]);
 }
